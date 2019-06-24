29c15816a21ae6b159ce7e2d625336f2e2de1a03
Andrew Ananda <andrewtiema@gmail.com>
2019-06-24 15:39:57 +0300

    register and permissions set

--- category.php
+++
@@ -1,9 +0,0 @@
-<?php include'connection.php';?>
-<?php include'layouts/header.php';?>
-<?php include'layouts/sections/sidebar.php';?>
-
-
-
-
-
-<?php include'layouts/footer.php';?>
--- register.php
+++ register.php
@@ -1,10 +1,34 @@
+<?php include'connection.php';?>
 <?php
-if(!isset($_SESSION))
+
+if(isset($_POST['submit']))
 {
-  session_start();
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
+  $password = md5($_POST['password']);
+  $user_type = $_POST['user_category_id'];
+  $password_confirm = md5($_POST['confirmPassword']);
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+
+  if($password == $password_confirm)
+  {
+    $insert = mysqli_query($conn,"INSERT INTO `users` (`first_name`,`last_name`,`password`,`user_type`,`email`,`status`,`phone`)" . " VALUES ('$first_name','$last_name','$password','$user_type','$email','1','$phone')");
+
+    echo "Successfully created";
+    header("Location: index.php");
+  }
+  else
+  {
+    die("error" . mysqli_connect_error($insert));
+  }
 }
 
-include'connection.php';?>
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -32,19 +56,19 @@ include'connection.php';?>
     <div class="card card-register mx-auto mt-5">
       <div class="card-header">Register an Account</div>
       <div class="card-body">
-        <form>
+        <form method="post" action="register.php">
           <div class="form-group">
             <div class="form-row">
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="firstName" class="form-control" placeholder="First name" required="required" autofocus="autofocus">
-                  <label for="firstName">First name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" placeholder="First name" required="required" autofocus="autofocus">
+                  <label for="first_name">First name</label>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="text" id="lastName" class="form-control" placeholder="Last name" required="required">
-                  <label for="lastName">Last name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" placeholder="Last name" required="required">
+                  <label for="last_name">Last name</label>
                 </div>
               </div>
             </div>
@@ -53,26 +77,24 @@ include'connection.php';?>
           <div class="form-row">
           <div class="col-md-6">
             <div class="form-label-group">
-              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required">
-              <label for="inputEmail">Email address</label>
+              <input type="email" id="email" name="email" class="form-control" placeholder="Email address" required="required">
+              <label for="email">Email address</label>
             </div>
             </div>
             <div class="col-md-6">
             <div class="form-label-group">
-              <select style="width:100%"; name="user_type" id="user_type">
+              <select style="width:100%"; name="user_category_id" id="user_category_id">
                 <option value="">--Register As--</option>
                 <?php 
                     $sql = mysqli_query($conn,"SELECT * FROM user_category");
-
-
-                      foreach($sql as $row)
-                      {
-                        ?>
-                        <option value="<?php $row['id'];?>"><?php echo $row['user_type'];?></option>
-                        <?php
-                      }
-
-                ?>
+                    if ($sql->num_rows > 0)
+                    {
+                    }
+                    ?>
+                     <?php foreach($sql as $row): ?>
+                        <option name="user_category_id" id="user_category_id" value="<?php echo $row['title'];?>"><?php echo $row['title'];?></option>
+                      
+                     <?php endforeach; ?>
 
               </select>
              
@@ -80,28 +102,40 @@ include'connection.php';?>
             </div>
           </div>
           </div>
+          
+          <div class="form-group">
+            <div class="form-row">
+            <div class="col-md-12">
+                <div class="form-label-group">
+                  <input type="text" name="phone" id="phone" class="form-control" placeholder="phone" required="phone">
+                  <label for="phone">phone</label>
+                </div>
+              </div>
+            </div>
+          </div>
 
           <div class="form-group">
             <div class="form-row">
+
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
-                  <label for="inputPassword">Password</label>
+                  <input type="password" name="password" id="password" class="form-control" placeholder="password" required="required">
+                  <label for="password">Password</label>
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-label-group">
-                  <input type="password" id="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
+                  <input type="password" id="confirmPassword" name="confirmPassword" class="form-control" placeholder="Confirm password" required="required">
                   <label for="confirmPassword">Confirm password</label>
                 </div>
               </div>
             </div>
           </div>
-          <a class="btn btn-primary btn-block" href="login.html">Register</a>
+          <button class="btn btn-primary btn-block" name="submit" id="submit">Submit</button>
         </form>
+        
         <div class="text-center">
           <a class="d-block small mt-3" href="index.php">Login Page</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
         </div>
       </div>
     </div>
@@ -117,3 +151,4 @@ include'connection.php';?>
 </body>
 
 </html>
+
